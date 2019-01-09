da194c924f93ebfbdea3ca15c42da6b035adb697
Bubble2 <guozhaodong318@163.com>
2019-01-09 16:58:31 +0800

    add demo3

---
+++ demo3/webpack.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    mode: 'none',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets:['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test:/\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+
+
+                // use: [
+                //     {
+                //         loader: 'style-loader',
+                //         options: {
+                //             sourceMap: true
+                //         }
+                //     },
+                //     {
+                //         loader: 'css-loader',
+                //         options: {
+                //             sourceMap: true
+                //         }
+                //     },
+                //     {
+                //         loader: 'sass-loader',
+                //         options: {
+                //             sourceMap: true
+                //         }
+                //     }
+                // ]
+            }
+        ]
+    }
+}
